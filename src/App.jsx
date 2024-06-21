36bf9e4ffe94711cee012c77821179f467ca85f9
Piyush-0605 <goluchoudhari411@gmail.com>
2024-06-21 21:03:20 +0530

    Quiz-application

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Quiz from './Components/Quiz/Quiz'
+
+function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  )
+}
+
+export default App
